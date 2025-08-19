e3bab3af4cb95d13818be6b8a6645917156f6e9c
Timofey <yakuvocixh96@mail.ru>
2025-08-19 19:31:17 +0300

    first_commit

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,10 @@
+// странциа по пути localhost:3000/product/1
+
+export default function ProductPage () {
+    return (
+        <div>
+            <h1>ProductPage</h1>
+            <h1>rsdgdere</h1>
+        </div>
+    )
+}
