cd5d56be90190e76751c1a55368ab9f0233f3d70
Timofey <yakuvocixh96@mail.ru>
2026-08-07 00:41:06 +0300

    vercel 2

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
   },
 });
