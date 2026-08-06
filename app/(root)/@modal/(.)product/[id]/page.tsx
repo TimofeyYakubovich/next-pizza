9702d1c3ec93f7639971e269e7a4b333ab323ec5
Timofey <yakuvocixh96@mail.ru>
2026-08-07 01:34:42 +0300

    vercel 5

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -3,7 +3,9 @@ import { prisma } from '@/prisma/prisma-client';
 import { ChooseProductModal } from '@/shared/components/shared';
 import { notFound } from 'next/navigation';
 
-export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
+// export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
+export default async function ProductModalPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
 
     const product = await prisma.product.findFirst({
         where: {
